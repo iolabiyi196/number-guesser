b8f1f7eaed2a1eefe4c5fefb5305ce00e7b6f33e
iolabiyi <79932461+iolabiyi@users.noreply.github.com>
2025-04-05 22:50:28 -0400

    first half done

--- script.js
+++ script.js
@@ -3,4 +3,36 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget() {
+  return Math.floor(Math.random() * 10);
+}
 
+function compareGuesses(humanGuess, computerGuess, targetGuess) {
+  if (humanGuess < 0 || humanGuess > 9) {
+    alert("You have entered an invalid number. Try again!");
+    return;
+  }
+  let humanDiff = Math.abs(humanGuess - targetGuess);
+  let computerDiff = Math.abs(computerGuess - targetGuess);
+
+  if (humanDiff < computerDiff) {
+    return true;
+  } else if (humanDiff === computerDiff) {
+    return true;
+  } else if (humanDiff > computerDiff) {
+    return false;
+  }
+}
+
+function updateScore(winner) {
+  winner.toLowerCase();
+  if (winner === "human") {
+    humanScore += 1;
+  } else if (winner === "computer") {
+    computerScore += 1;
+  }
+}
+
+function advanceRound() {
+  currentRoundNumber += 1;
+}
